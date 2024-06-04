49ac42a740a0a5ec15e6aad0e5aa15ec511f6b6a
Nick <nickphilomath@gmail.com>
2024-06-04 10:20:12 -0400

    add demo code for bot

--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -1,21 +1,23 @@
 from django.core.management.base import BaseCommand, CommandError
 
+from bot.main import main
 
 class Command(BaseCommand):
     help = 'Describe your command here'
 
-    def add_arguments(self, parser):
-        # Define command-line arguments here
-        parser.add_argument('arg1', type=str, help='Argument 1 description')
-        parser.add_argument('--optional_arg', type=int, help='Optional argument description', default=42)
+    # def add_arguments(self, parser):
+    #     # Define command-line arguments here
+    #     parser.add_argument('arg1', type=str, help='Argument 1 description')
+    #     parser.add_argument('--optional_arg', type=int, help='Optional argument description', default=42)
 
     def handle(self, *args, **options):
-        arg1 = options['arg1']
-        optional_arg = options['optional_arg']
+        main()
+        # arg1 = options['arg1']
+        # optional_arg = options['optional_arg']
 
-        # Implement your custom command logic here
-        self.stdout.write(self.style.SUCCESS(f'Argument 1: {arg1}'))
-        self.stdout.write(self.style.SUCCESS(f'Optional Argument: {optional_arg}'))
+        # # Implement your custom command logic here
+        # self.stdout.write(self.style.SUCCESS(f'Argument 1: {arg1}'))
+        # self.stdout.write(self.style.SUCCESS(f'Optional Argument: {optional_arg}'))
 
         # You can raise a CommandError to indicate something went wrong
         # if some_condition:
